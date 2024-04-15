8c2b1e35c79ea390c81afe0a5bbc875173e9fd4c
Evan Gross <egross6142@stu.d214.org>
2024-04-15 14:49:22 -0500

    MadLib page

---
+++ MadLib.swift
@@ -0,0 +1,20 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Evan Gross on 4/15/24.
+//
+
+import SwiftUI
+
+
+
+struct MadLib: View {
+    @State var noun = ""
+    @State var verb = ""
+    @State var adjective = ""
+    var body: some View {
+        Text("Hi")
+    }
+}
+
